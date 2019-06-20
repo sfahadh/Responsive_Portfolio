bc96a570c319bf5bb3466b9177f59f69bdca33a9
Fahad Hussain <sfahadh14@gmail.com>
2019-06-20 16:02:27 -0400

    added divs in navbar and cleaned some css code in navbar syle

--- src/components/Navbar/ShowNav.js
+++ src/components/Navbar/ShowNav.js
@@ -13,19 +13,31 @@ class ShowNav extends Component {
     
     render() {
         let navClass = !this.props.reveal ? "side-drawer" : "side-drawer slide-action"
+
         return (
             <div>
                 <nav className={navClass}>
                     {/* <img id="logo-image" alt="my new logo" src={require("../../Images/logo.png")}/> */}
                     <ul className='nav-links'>
-                        <h4><Link to="/"><FontAwesomeIcon icon={faHome}/></Link></h4>
-                        <h6 id="home">Home</h6>
-                        <h4><Link to="about-me"><FontAwesomeIcon icon={faUser}/></Link></h4>
-                        <h6 id="about">About Me</h6>
-                        <h4><Link to="projects"><FontAwesomeIcon icon={faEye}/></Link></h4>
-                        <h6 id="projects">Projects</h6>
-                        <h4><Link to="contact"><FontAwesomeIcon icon={faAddressBook}/></Link></h4>
-                        <h6 id="contact">Contact</h6>
+                        <div className="icon-badge">
+                            <h4><Link to="/"><FontAwesomeIcon icon={faHome}/></Link></h4>
+                            <h6 id="home">Home</h6>
+                        </div>
+
+                        <div className="icon-badge">
+                            <h4><Link to="about-me"><FontAwesomeIcon icon={faUser}/></Link></h4>
+                            <h6 id="about">About Me</h6>
+                        </div>
+
+                        <div className="icon-badge">
+                            <h4><Link to="projects"><FontAwesomeIcon icon={faEye}/></Link></h4>
+                            <h6 id="projects">Projects</h6>
+                        </div>
+
+                       <div className="icon-badge">
+                            <h4><Link to="contact"><FontAwesomeIcon icon={faAddressBook}/></Link></h4>
+                            <h6 id="contact">Contact</h6>
+                       </div>
                     </ul>
                 </nav>
             </div>
